a10adc3a9877ece09423a69eef609ef6025ce02a
Daniele <daniele.rb6@gmail.com>
2018-09-08 18:20:45 +0200

    feat: Add bodyParser middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { restRouter } from './api/restRouter';
+import setupMiddleware from './middleware';
 
 const app = express();
+setupMiddleware(app);
 
 app.get('/', (req, res) => {
   res.send('Basic routing setting is ok!');
